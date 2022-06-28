0e65a41e19b7dd756e048b139f34be3c6d7da49f
pawan vannal <pawanvannal1993@gmail.com>
2022-06-28 13:53:20 +0530

    commiting lab-5 sloution

---
+++ StudentManagementSystem/src/main/java/com/greatlearning/javafsd/studentmanagement/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.greatlearning.javafsd.studentmanagement.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+	@RequestMapping("/")
+	public String handleWelcome() {
+		return "welcome";
+
+	}
+
+}
